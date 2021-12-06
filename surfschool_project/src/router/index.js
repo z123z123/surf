aaf199cd45cb74f36ace7188e14b0c1e679d5ad9
Tristan Veskilt <tristanveskilt@gmail.com>
2021-12-06 15:10:11 +0200

    Backend setup + vuetify

--- surfschool_project/src/router/index.js
+++ surfschool_project/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import BookingSingle from "@/views/BookingSingle";
 
 Vue.use(VueRouter)
 
@@ -19,6 +20,11 @@ const routes = [
     component: function () {
       return import(/* webpackChunkName: "about" */ '../views/About.vue')
     }
+  },
+  {
+    path: "/booking",
+    name: "BookingSingle",
+    component: BookingSingle
   }
 ]
 
