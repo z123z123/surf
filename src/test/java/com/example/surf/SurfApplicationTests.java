87a92d95fbb325967b931f4e858145641d89f742
Õpilane <opilane@ak.sise>
2021-12-06 10:23:38 +0200

    Initial commit

---
+++ src/test/java/com/example/surf/SurfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.surf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
